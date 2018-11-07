aaa5a490966a25dbaa0728aeeba998c01f16975b
Samson Weiser <Samson Weiser>
2018-11-06 22:49:26 -0500

    Instead of a delay at the end it does the right thing and times out videos at 10 minutes

--- dance.py
+++ dance.py
@@ -4,5 +4,7 @@ import io, sys, os, glob, subprocess, time, random
 vid_list = glob.glob('*.mp4')
 while True:
     video_num = random.randint(0, len(vid_list)-1)
-    subprocess.call(['vlc', vid_list[video_num], '--play-and-exit'])
-    time.sleep(600)
+    try:
+        subprocess.run(['vlc', vid_list[video_num], '--play-and-exit'], timeout = 600)
+    except subprocess.TimeoutExpired:
+        pass
